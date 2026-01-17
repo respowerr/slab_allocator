a28a6914835274b361a334b01ba709b5fbd79de8
respowerr <respowerr@callidosgroup.eu>
2026-01-17 13:24:19 +0100

    Use Spin/Mutex for Allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,59 +1,76 @@
 use crate::cache::SCache;
 use core::alloc::{GlobalAlloc, Layout};
-use core::ptr::NonNull;
+use core::ptr::null_mut;
+use spin::Mutex;
 
 const CACHE_SIZES: [usize; 8] = [8, 16, 32, 64, 128, 256, 512, 1024];
 const MAX_CACHE_SIZE: usize = 1024;
 
 pub struct SlabAllocator {
-    caches: [SCache; 8],
+    caches: [Mutex<SCache>; 8],
 }
 
 impl SlabAllocator {
     pub const fn new() -> Self {
         Self {
             caches: [
-                SCache::new(8),
-                SCache::new(16),
-                SCache::new(32),
-                SCache::new(64),
-                SCache::new(128),
-                SCache::new(256),
-                SCache::new(512),
-                SCache::new(1024),
+                Mutex::new(SCache::new(8)),
+                Mutex::new(SCache::new(16)),
+                Mutex::new(SCache::new(32)),
+                Mutex::new(SCache::new(64)),
+                Mutex::new(SCache::new(128)),
+                Mutex::new(SCache::new(256)),
+                Mutex::new(SCache::new(512)),
+                Mutex::new(SCache::new(1024)),
             ],
         }
     }
 
-    fn select_cache(&mut self, size: usize) -> Option<&mut SCache> {
+    fn get_cache_index(&self, size: usize) -> Option<usize> {
         for (i, &cache_size) in CACHE_SIZES.iter().enumerate() {
             if size <= cache_size {
-                return Some(&mut self.caches[i]);
+                return Some(i);
             }
         }
         None
     }
 
-    pub fn allocate(&mut self, layout: Layout) -> Option<NonNull<u8>> {
+    pub fn object_size_for(&self, layout: Layout) -> Option<usize> {
         let size = layout.size();
         if size > MAX_CACHE_SIZE {
             return None;
         }
-        
-        let cache = self.select_cache(size)?;
-        cache.alloc()
-    }
-
-    pub fn object_size_for(&self, layout: Layout) -> Option<usize> {
-        let size = layout.size();
-        CACHE_SIZES.iter().find(|&&cs| size <= cs).copied()
+        for &cache_size in CACHE_SIZES.iter() {
+            if size <= cache_size {
+                return Some(cache_size);
+            }
+        }
+        None
     }
 }
 
 unsafe impl GlobalAlloc for SlabAllocator {
-    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
-        core::ptr::null_mut()
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let size = layout.size();
+        
+        if size > MAX_CACHE_SIZE {
+            return null_mut();
+        }
+
+        let cache_idx = match self.get_cache_index(size) {
+            Some(idx) => idx,
+            None => return null_mut(),
+        };
+
+        let mut cache = self.caches[cache_idx].lock();
+
+        if let Some(ptr) = cache.alloc() {
+            return ptr.as_ptr();
+        }
+
+        null_mut()
     }
 
-    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+    }
 }
