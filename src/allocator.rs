0de2fc3996d84a0c28f2fdb17f18fd809240ec4f
respowerr <respowerr@callidosgroup.eu>
2026-01-17 18:20:48 +0100

    Add page allocator and refill logic

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,10 +1,29 @@
 use crate::cache::SCache;
 use core::alloc::{GlobalAlloc, Layout};
-use core::ptr::null_mut;
+use core::ptr::{NonNull, null_mut};
 use spin::Mutex;
 
 const CACHE_SIZES: [usize; 8] = [8, 16, 32, 64, 128, 256, 512, 1024];
 const MAX_CACHE_SIZE: usize = 1024;
+const HEAP_SIZE: usize = 1024 * 1024;
+static mut HEAP_MEMORY: [u8; HEAP_SIZE] = [0; HEAP_SIZE];
+static mut HEAP_INDEX: usize = 0;
+
+unsafe fn alloc_page() -> *mut u8 {
+    let heap_ptr = core::ptr::addr_of_mut!(HEAP_MEMORY) as *mut u8;
+    let current_ptr = heap_ptr.add(HEAP_INDEX);
+    let align_offset = current_ptr.align_offset(4096);
+    let start_index = HEAP_INDEX + align_offset;
+    let end_index = start_index + 4096;
+
+    if end_index > HEAP_SIZE {
+        return null_mut();
+    }
+
+    HEAP_INDEX = end_index;
+    heap_ptr.add(start_index)
+}
+
 
 pub struct SlabAllocator {
     caches: [Mutex<SCache>; 8],
@@ -34,6 +53,29 @@ impl SlabAllocator {
         }
         None
     }
+
+    unsafe fn refill(&self, cache_idx: usize) -> bool {
+        let page_ptr = alloc_page();
+        if page_ptr.is_null() {
+            return false;
+        }
+
+        let slab_ptr = page_ptr as *mut crate::slab::Slab;
+        let slab_size = core::mem::size_of::<crate::slab::Slab>();
+        let memory_start = page_ptr.add(slab_size);
+        let available_memory = 4096 - slab_size;
+        let object_size = CACHE_SIZES[cache_idx];
+
+        core::ptr::write(slab_ptr, crate::slab::Slab::new(object_size, available_memory));
+        
+        (*slab_ptr).init(NonNull::new_unchecked(memory_start));
+
+        let slab_ref = &mut *slab_ptr;
+        self.caches[cache_idx].lock().insert(slab_ref);
+
+        true
+    }
+
 
     pub fn object_size_for(&self, layout: Layout) -> Option<usize> {
         let size = layout.size();
@@ -66,6 +108,15 @@ unsafe impl GlobalAlloc for SlabAllocator {
 
         if let Some(ptr) = cache.alloc() {
             return ptr.as_ptr();
+        }
+
+        drop(cache); 
+
+        if self.refill(cache_idx) {
+            let mut cache = self.caches[cache_idx].lock();
+            if let Some(ptr) = cache.alloc() {
+                return ptr.as_ptr();
+            }
         }
 
         null_mut()
